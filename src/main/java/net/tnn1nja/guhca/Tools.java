1c6012044d7166ae8dcc771bf8d6c76fc41f0498
tnn1nja <thenonsenseninja@gmail.com>
2025-01-05 21:54:22 +0000

    Fixed I think, more testing needed.

--- src/main/java/net/tnn1nja/guhca/Tools.java
+++ src/main/java/net/tnn1nja/guhca/Tools.java
@@ -181,11 +181,11 @@ public class Tools {
     public static void doCrystalRelocateAnim(Player p){
         World w = Bukkit.getWorlds().get(0);
         w.playSound(p.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1F, 1F);
-        w.spawnParticle(Particle.DUST, p.getLocation().add(0.5, (int) p.getEyeHeight(),
-                        0.5), 2048, 0.55, 0.55, 0.55, 1,
+        w.spawnParticle(Particle.DUST, p.getLocation().add(0, (int) p.getEyeHeight(),
+                        0), 2048, 0.55, 0.55, 0.55, 1,
                 new Particle.DustOptions(Color.fromRGB(252, 47, 72), 1.2F), true);
-        w.spawnParticle(Particle.DUST, p.getLocation().add(0.5, p.getEyeHeight(),
-                        0.5), 256, 0.1, 0.1, 0.1, 1,
+        w.spawnParticle(Particle.DUST, p.getLocation().add(0, p.getEyeHeight(),
+                        0), 256, 0.1, 0.1, 0.1, 1,
                 new Particle.DustOptions(Color.fromRGB(252, 47, 72), 1.2F), true);
     }
 
